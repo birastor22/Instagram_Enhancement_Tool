5a3b83cfd0c99d4ebe7f3a2f9930fa18b30c8c06
Ben Irastorza <114950579+birastor22@users.noreply.github.com>
2024-02-14 08:38:00 -0500

    Updated Program Efficiency.

--- Tool.java
+++ Tool.java
@@ -3,17 +3,19 @@
  * @version 1.1337
  */
 /*
-    * The program does NOT require you surrender your instagram username and password.
+    * The program does NOT require you to surrender your instagram username and password.
     * The program also is all ran locally and securely on your personal computer.
-    * The user is only required to input through the command line
+    * The user is only required to input through the command line.
 
     * Step 1: Download your Instagram data per the instructions on https://help.instagram.com/181231772500920
+    *  MAKE SURE TO CHOOSE "JSON" FILE FORMAT AND SELECT "ALL TIME" DATA
     * Step 2: Get the path of the Following and Followers json files under "followers_and_following"
-    * Step 3: Create an empty .txt file and copy the file path.
-    * Step 4: Run the program and paste the 3 required file paths when requested.
+    * Step 3: Create an empty .txt file and copy the file path
+    * Step 4: Run the program and paste the 3 required file paths when requested
  */
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.PrintWriter;
@@ -48,7 +50,7 @@ public class Tool {
     }
 
     /**
-     * The following method parses the JSON file and creates a list of the users following.
+     * followingToArr parses the JSON file and creates a list of the users following.
      * @param filePath the path to the following file
      * @return the list of following
      */
@@ -60,7 +62,7 @@ public class Tool {
             Scanner scan = new Scanner(x);
             while (scan.hasNextLine()) {
                 currLine = scan.nextLine();
-                if (currLine.indexOf("value") != -1) {
+                if (currLine.contains("value")) {
                     currLine = currLine.substring(20, currLine.length() - 2);
                     toReturn.add(currLine);
                 }
@@ -72,7 +74,7 @@ public class Tool {
     }
 
     /**
-     * The following method parses the JSON file and creates a list of the users followers.
+     * followersToArr parses the JSON file and creates a list of the users followers.
      * @param filePath the path to the followers file
      * @return the list of followers
      */
@@ -84,7 +86,7 @@ public class Tool {
             Scanner scan = new Scanner(x);
             while (scan.hasNextLine()) {
                 currLine = scan.nextLine();
-                if (currLine.indexOf("value") != -1) {
+                if (currLine.contains("value")) {
                     currLine = currLine.substring(18, currLine.length() - 2);
                     toReturn.add(currLine);
                 }
@@ -97,37 +99,32 @@ public class Tool {
 
     /**
      * The following method loops through both lists adding the flagged usernames to a new list.
+     * Optimization: updated runtime to O(n)
      * @param following the list of following
      * @param followers the list of followers
      * @return the list of people not following the user back
      */
     public static ArrayList<String> notFollowBack(ArrayList<String> following, ArrayList<String> followers) {
         ArrayList<String> answer = new ArrayList<>();
-        boolean flag = false;
-        for (int i = 0; i < following.size(); i++) {
-            flag = false;
-            for (int j = 0; j < followers.size(); j++) {
-                if (following.get(i).equals(followers.get(j))) {
-                    flag = true; //they follow you black
-                }
-            }
-            if (!flag) {
-                answer.add(following.get(i));
+        HashSet<String> followersSet = new HashSet<>(followers);
+        for (String s : following) {
+            if (!followersSet.contains(s)) {
+                answer.add(s);
             }
         }
         return answer;
     }
 
     /**
-     * The following method writes the data in the list to a text file for the user to have.
+     * writeToFile writes the data to a text file for the user to have.
      * @param x the file the user chose to have the data written to.
      * @param answer the list of the people that don't follow the user back.
      */
     public static void writeToFile(File x, ArrayList<String> answer) {
         try {
             PrintWriter pw = new PrintWriter(x);
-            for (int i = 0; i < answer.size(); i++) {
-                pw.println(answer.get(i));
+            for (String s : answer) {
+                pw.println(s);
             }
             pw.close();
             System.out.println("The data has successfully been written to the text file.");
